f7e7a8d844e07ea0bfbcc8ec21028c948e1a7e4a
Ondrej Smid <ondrejsmid33@gmail.com>
2021-10-23 22:38:14 +0200

    service and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataComponent } from './data/data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: DataComponent },
+  { path: "**", redirectTo: "" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
