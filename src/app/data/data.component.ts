dc748cfee3f7e7261fa1bdb673a9296a35cfc5c8
Ondrej Smid <ondrejsmid33@gmail.com>
2021-10-25 10:20:17 +0200

    display data for one slice

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseStation, Provision, Slice, Timesteps } from '../modules/api.module';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-data',
@@ -7,9 +9,121 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
-  constructor() { }
+  public window!: number;
+  public time!: number;
+  public slice!: number;
 
-  ngOnInit(): void {
+  public base_stations!: Slice;
+  public bs_number!: number;
+  public timesteps!: Timesteps;
+  public slice_number!: number;
+  public slice_base_stations!: Array<number>;
+
+  public provision!: Array<Provision>
+
+
+  constructor(private apiService: DataService) { }
+
+  async ngOnInit() {
+    await this.fetchAll();
   }
 
+  async fetchAll() {
+    Promise.all([
+      this.getTime(),
+      this.getBSList(),
+    ]);
+  }
+
+  getBSList() {
+    const promise = new Promise((resolve, reject) =>  {
+      this.apiService.getListOfBS().toPromise().then(
+        (res: any) => {
+          this.base_stations = res;
+          this.bs_number = res.base_station.length
+          console.log(this.bs_number)
+          this.slice_number = this.maxSlice(res.base_station);
+          console.log(this.slice_number)
+        }
+      )
+    })
+    return promise;
+  }
+
+  maxSlice(base_stations: Array<BaseStation>) {
+    let maximum = 0;
+    base_stations.forEach(function(bs) {
+      if (bs.slice_id > maximum) {
+        maximum = bs.slice_id
+      }
+    }); 
+    return maximum + 1;
+  }
+
+  getTime() {
+    const promise = new Promise((resolve, reject) =>  {
+      this.apiService.getTimesptes(this.time, this.window).toPromise().then(
+        (res: any) => {
+          this.timesteps = res;
+        }
+      )
+    })
+    return promise;
+  }
+
+  async getSliceProv() {
+    Promise.all([this.getSlice()]).then(
+      (res: any) => {
+        this.getBSProv(this.slice_base_stations[0])
+      }
+    )
+  }
+
+  getSlice() {
+    const promise = new Promise((resolve, reject) =>  {
+      this.apiService.getSlice(this.slice).toPromise().then(
+        (res: any) => {
+          let tmp = res;
+          let array = new Array<number>();
+          tmp.base_station.forEach(function(bs: BaseStation) {
+            array.push(bs.id);
+          });
+          this.slice_base_stations = array;
+          for (let i = 0; i < this.slice_base_stations.length; ++i) {
+            this.getBSProv(this.slice_base_stations[i])
+          }
+        }
+      )
+    })
+    return promise;
+  }
+
+  getBSProv(bs_id: number) {
+    const promise = new Promise((resolve, reject) =>  {
+      this.apiService.getProvision(bs_id, this.time, this.window).toPromise().then(
+        (res: any) => {
+          if (this.provision === undefined) {
+            this.provision = new Array<Provision>();
+          }
+          this.provision.push(res);
+        }
+      )
+    })
+    return promise;
+  }
+
+  setParam() {
+    if (this.slice !== undefined && this.slice !== null) {
+      if (this.provision !== undefined) {
+        this.provision = new Array<Provision>();
+      }
+      Promise.all([
+        this.getTime(),
+        this.getSliceProv()
+      ]);
+    }
+    else {
+      this.getTime();
+    }
+  }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
-  degListOfBS() {
+  getListOfBS() {
     console.log("call API to get list of BS @ " + this.url + "/bs")
     return this.http.get(this.url + "/bs")
       .pipe((response) => response);
@@ -28,35 +29,49 @@ export class DataService {
       .pipe((response) => response);
   }
 
-  getProvision(bs_id: number, time: number, window: number) {
-    if (window === undefined){
+  getProvision(bs_id: number, time?: number, window?: number) {
+    if ((window === undefined || window === null) && (time !== undefined && time !== null)) {
       console.log("call API to get Provision of BS" + bs_id + " from time " + time + " @ " + this.url + "/provision/" + bs_id + "/time=" + time)
       return this.http.get(this.url + "/provision/" + bs_id + "/time=" + time)
         .pipe((response) => response);
     }
-    if (time === undefined && window === undefined) {
+    else if ((window !== undefined && window !== null) && (time === undefined || time === null)) {
+      console.log("call API to get Provision of BS" + bs_id + " from time " + 0 + " for window " + window + " @ " + this.url + "/provision/" + bs_id + "/time=" + 0 + "/window=" + window)
+      return this.http.get(this.url + "/provision/" + bs_id + "/time=" + 0 + "/window=" + window)
+        .pipe((response) => response);
+    }
+    else if ((time === undefined || time === null) && (window === undefined || window === null)) {
+      console.log("call API to get Provision of BS" + bs_id + " @ " + this.url + "/provision/" + bs_id)
+      return this.http.get(this.url + "/provision/" + bs_id)
+        .pipe((response) => response);
+    }
+    else {
       console.log("call API to get Provision of BS" + bs_id + " from time " + time + " for window " + window + " @ " + this.url + "/provision/" + bs_id + "/time=" + time + "/window=" + window)
       return this.http.get(this.url + "/provision/" + bs_id + "/time=" + time + "/window=" + window)
         .pipe((response) => response);
     }
-    console.log("call API to get Provision of BS" + bs_id + " @ " + this.url + "/provision/" + bs_id)
-    return this.http.get(this.url + "/provision/" + bs_id)
-      .pipe((response) => response);
   }
 
-  getTimesptes(time: number, window: number) {
-    if (window === undefined){
+  getTimesptes(time?: number, window?: number): Observable<any> {
+    if ((time === undefined || time === null) && (window === undefined || window === null)) {
+      console.log("call API to get Timestep @ " + this.url + "/timesteps")
+      return this.http.get(this.url + "/timesteps")
+        .pipe((response: any) => {return response});
+    }
+    else if ((window === undefined || window === null) && (time !== undefined && time !== null)) {
       console.log("call API to get Timestep from time " + time + " @ " + this.url + "/timesteps/time=" + time)
       return this.http.get(this.url + "/timesteps/time=" + time)
         .pipe((response) => response);
     }
-    if (time === undefined && window === undefined) {
+    else if ((window !== undefined && window !== null) && (time === undefined || time === null)) {
+      console.log("call API to get Timestep from time " + 0 + " for window " + window + " @ " + this.url + "/timesteps/time=" + 0 + "/window=" + window)
+      return this.http.get(this.url + "/timesteps/time=" + 0 + "/window=" + window)
+        .pipe((response) => response);
+    }
+    else {
       console.log("call API to get Timestep from time " + time + " for window " + window + " @ " + this.url + "/timesteps/time=" + time + "/window=" + window)
       return this.http.get(this.url + "/timesteps/time=" + time + "/window=" + window)
         .pipe((response) => response);
     }
-    console.log("call API to get Timestep @ " + this.url + "/timesteps")
-    return this.http.get(this.url + "/timesteps")
-      .pipe((response) => response);
   }
 }
